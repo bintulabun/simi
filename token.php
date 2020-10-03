c334653f1e3617f0dc761c35c2c8258701855fb0
bintulabun <72130654+bintulabun@users.noreply.github.com>
2020-10-03 08:20:10 +0800

    Create token.php

---
+++ token.php
@@ -0,0 +1,15 @@
+<?php
+
+/* ganti xxx dengan token kamu */
+$token = "xxxx";
+
+/* ganti yyy dengan id Telegram kamu */
+$owner_id = "yyy";
+
+/* 
+
+NB: cara nengetahui id Telegram kamu, buka t.me/StrukturBot 
+
+*/
+
+?>
